fcc08b063d81acc7bfe8c62823388d95413fcbd2
soubj <sobujahmed124@gmail.com>
2023-05-27 23:28:17 +0600

    pusing new user to existing users

--- index.js
+++ index.js
@@ -25,6 +25,10 @@ app.get("/users", (req, res) => {
 app.post("/users", (req, res) => {
   console.log("users hitting");
   console.log(req.body);
+  const newUser = req.body;
+  newUser.id = users.length + 1;
+  users.push(newUser);
+  res.send(users);
 });
 
 app.listen(port, () => {
